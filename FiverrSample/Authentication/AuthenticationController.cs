7c49a60356000c0f4a9ab57e9bbbfc9f5aaca606
Hasitha Wikramasinghe <hasitha.softworxio@gmail.com>
2023-11-04 12:56:02 +0530

    Sign in page UI

---
+++ FiverrSample/Authentication/AuthenticationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiverr_Sample.Authentication
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn() 
+        {
+            return View();
+        }
+    }
+}
