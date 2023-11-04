35df3d1fae73f4959ade308d2b52806a5e45b1ce
Hasitha Wikramasinghe <78442062+hasitha-wikramasinghe@users.noreply.github.com>
2023-11-04 16:21:54 +0530

    Feature/authentication implementation (#1)
    * Sign in page UI
    
    * Basic authentication
    
    ---------
    
    Co-authored-by: Hasitha Wikramasinghe <hasitha.softworxio@gmail.com>

---
+++ FiverrSample/Authentication/AuthenticationController.cs
@@ -0,0 +1,56 @@
+﻿using Fiverr_Sample.Authentication.Models;
+using Fiverr_Sample.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiverr_Sample.Authentication
+{
+    public class AuthenticationController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signinManager;
+
+        public AuthenticationController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signinManager)
+        {
+            _userManager = userManager;
+            _signinManager = signinManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInModel signInModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.FindByEmailAsync(signInModel.Email);
+            if (user is null) 
+            {
+                return BadRequest($"Cannot find a user for email {signInModel.Email}");
+            }
+
+            var signinResult = await _signinManager.PasswordSignInAsync(user, signInModel.Password, true, false);
+
+            if (signinResult.Succeeded)
+            {
+                return RedirectToAction("Index", "FoodOrder");
+            }
+
+            return RedirectToAction("SignIn");
+        }
+    }
+}
---
+++ FiverrSample/Authentication/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Fiverr_Sample.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiverr_Sample.Authentication
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signinManager;
+
+        public UserController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signinManager)
+        {
+            _userManager = userManager;
+            _signinManager = signinManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ApplicationUser applicationUser)
+        {
+            // ATM using email as the user name 
+            applicationUser.UserName = applicationUser.Email;
+            applicationUser.EmailConfirmed = true;
+
+            var result = await _userManager.CreateAsync(applicationUser, "Hotelio@123");
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Signin", "Authentication");
+            }
+
+            return RedirectToAction("Create", "User");
+        }
+    }
+}
