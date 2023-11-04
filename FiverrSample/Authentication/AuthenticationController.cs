b1a8dc7d5775dea0b6856d5ac9222389cf6e5150
Hasitha Wikramasinghe <hasitha.softworxio@gmail.com>
2023-11-04 15:49:51 +0530

    Basic authentication

--- FiverrSample/Authentication/AuthenticationController.cs
+++ FiverrSample/Authentication/AuthenticationController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiverr_Sample.Authentication.Models;
+using Fiverr_Sample.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fiverr_Sample.Authentication
 {
     public class AuthenticationController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signinManager;
+
+        public AuthenticationController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signinManager)
+        {
+            _userManager = userManager;
+            _signinManager = signinManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -13,6 +27,30 @@ namespace Fiverr_Sample.Authentication
         public IActionResult SignIn() 
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInModel signInModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.FindByEmailAsync(signInModel.Email);
+            if (user is null) 
+            {
+                return BadRequest($"Cannot find a user for email {signInModel.Email}");
+            }
+
+            var signinResult = await _signinManager.PasswordSignInAsync(user, signInModel.Password, true, false);
+
+            if (signinResult.Succeeded)
+            {
+                return RedirectToAction("Index", "FoodOrder");
+            }
+
+            return RedirectToAction("SignIn");
         }
     }
 }
---
+++ FiverrSample/Authentication/Models/SignInModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiverr_Sample.Authentication.Models
+{
+    public class SignInModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
