73859f75fb6134095e00a6af3c0684b7bb208ade
Hasitha Wikramasinghe <hasitha@corzent.com>
2023-12-01 17:48:15 +0530

    Change solution name Change project name Change namespaces

---
+++ RestaurantManagement/Controllers/FoodOrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantManagement.Controllers
+{
+    public class FoodOrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RestaurantManagement/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using RestaurantManagement.Authentication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantManagement.DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
